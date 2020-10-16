d1febf49831b32df0bc1bc7ebcb0b9f3edabfa8d
Jay Kamat <jaygkamat@gmail.com>
2020-10-16 10:36:47 -0700

    Add hack to treat references as pointers

--- libdrgn/debug_info.c
+++ libdrgn/debug_info.c
@@ -2338,6 +2338,7 @@ drgn_type_from_dwarf_internal(struct drgn_debug_info *dbinfo,
 						   &ret->type);
 		break;
 	case DW_TAG_pointer_type:
+	case DW_TAG_reference_type:
 		err = drgn_pointer_type_from_dwarf(dbinfo, die, bias, lang,
 						   &ret->type);
 		break;
